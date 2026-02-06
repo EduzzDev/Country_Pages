704571c08c9b94445ef43e62d9a4f122395012e9
Eduardo <eduzzfelipe21@gmail.com>
2026-02-06 17:48:59 -0300

    feat: Adicionando uma versão mobile para o Countries details

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -38,12 +38,13 @@ function CountryDetails() {
         />
         <img className="absolute  w-50" src={logoSvg} alt="Logo" />
       </header>
-      <main className="w-full  flex justify-center  mx-auto px-4 font-[Be_Vietnam_Pro, sans-serif] bg-[#1B1D1F]">
-        <div className="md:w-160 w-screen text-[#D2D5DA] justify-center overflow-x-hiddenflex relative bottom-15 flex-col items-center  text-center left-[2dvw] h-390 lg:h-260  shadow-2xl border-double border border-[#282B30] rounded-[15px] p-8 bg-[#1B1D1F]">
+      <main className="w-full flex justify-center  mx-auto px-4 font-[Be_Vietnam_Pro, sans-serif] bg-[#1B1D1F]">
+        <div className="min-[600px]:w-160 w-full text-[#D2D5DA] justify-center overflow-x-hiddenflex relative bottom-15 flex-col
+         items-center  text-center left-[2dvw] h-390 lg:h-260  shadow-2xl border-double border border-[#282B30] rounded-[15px] p-8 bg-[#1B1D1F]">
           <img
             src={flag}
             alt={`Flags of S${nameContry}`}
-            className="w-65 max-[480px]:w-45 max-[480px]:h-30 max-[390px]:left-[12dvw] h-45 relative bottom-20 left-[26%]  object-cover border-0 rounded-lg "
+            className="min-[600px]:w-65 w-[52dvw] h-30  min-[600px]:h-45 relative bottom-20 min-[600px]:left-[26%] left-[15%]   object-cover border-0 rounded-lg "
           />
           <h1 className="bottom-15 text-[32px]  font-bold relative">
             {nameContry}
@@ -51,16 +52,16 @@ function CountryDetails() {
           <h2 className="text-[14px] bottom-15 relative font-bold  ">
             {nameOfficial}
           </h2>
-          <div className="flex md:gap-x-10 max-[500px]:gap-1">
-            <p className=" text-[14px] md:w-60 max-w-[500px]:w-42 md:p-2  max-[500px]:ml-0 flex md:-m-6 relative font-semibold justify-center items-center md:ml-8  bg-[#282B30] rounded-xl">
+          <div className="flex min-[600px]:flex-row flex-col min-[600px]:gap-x-10 gap-y-2">
+            <p className=" text-[14px] min-[600px]:w-60 p-2  max-[500px]:ml-0 flex md:-m-6 relative font-semibold justify-center items-center md:ml-8  bg-[#282B30] rounded-xl">
               Population{" "}
-              <span className="w-px max-[500px]:w-[0.1px]  max-[500px]:mr-0 max-[500px]:ml-2 font-black inline-block h-8 mr-3 ml-4 bg-black"></span>{" "}
+              <span className="w-px max-[500px]:w-[0.1px]  font-black inline-block h-8 mr-3 ml-4 bg-black"></span>{" "}
               <span className="ml-1 truncate">{population}</span>
             </p>
-            <p className=" text-[14px] md:w-60 max-w-[500px]:w-42 md:p-2  max-[500px]:ml-0 flex md:-m-6 relative font-semibold justify-center items-center md:ml-8  bg-[#282B30] rounded-xl">
+            <p className=" text-[14px]  min-[600px]:w-60 p-2  flex md:-m-6 relative font-semibold justify-center items-center md:ml-8  bg-[#282B30] rounded-xl">
               Area{"(km²) "}
-              <span className="w-px max-[500px]:w-[0.1px]  max-[500px]:mr-0 max-[500px]:ml-4 font-black inline-block h-8 mr-3 ml-4 bg-black"></span>{" "}
-              <span className="ml-1 max-[500px]:ml-4  truncate">{area}</span>
+              <span className="w-px max-[500px]:w-[0.1px]  font-black inline-block h-8 mr-3 ml-4 bg-black"></span>{" "}
+              <span className="ml-1 ">{area}</span>
             </p>
           </div>
           <div className="flex justify-between items-center p-4 top-10 w-full relative border-[#282B30] border-t border-b">
@@ -68,21 +69,21 @@ function CountryDetails() {
             <span>{capital}</span>
           </div>
           <div className="flex justify-between items-center p-4 top-10 w-full relative border-[#282B30] border-t border-b">
-            <span className="font-medium">Subregion</span>
+            <span className="font-medium min-[600px]:mr-0 mr-5">Subregion</span>
             <span>{subregion}</span>
           </div>
           <div className="flex justify-between items-center p-4 top-10 w-full relative border-[#282B30] border-t border-b">
-            <span className="font-medium">Language</span>
+            <span className="font-medium min-[600px]:mr-0 mr-5">Language</span>
             <span className="tracking-tigh flex gap-2">
               {languages.join(", ")}
             </span>
           </div>
           <div className="flex justify-between items-center p-4 top-10 w-full relative border-[#282B30] border-t border-b">
-            <span className="font-medium">Currencies</span>
+            <span className="font-medium min-[600px]:mr-0 mr-5">Currencies</span>
             <span className="tracking-tigh flex gap-2">{currencies}</span>
           </div>
           <div className="flex justify-between items-center p-4 top-10 w-full relative border-[#282B30] border-t border-b">
-            <span className="font-medium">Continents</span>
+            <span className="font-medium min-[600px]:mr-0 mr-5">Continents</span>
             <span className="tracking-tigh flex gap-2">{continents}</span>
           </div>
           <div className="flex flex-col justify-between items-baseline  p-4 top-10 w-full relative border-[#282B30] border-t border-b">
